06805362244f290ad6d261d7f53c60b7f5bb2848
Zafchiel <bialakropla127.9@o2.pl>
2023-06-18 23:56:40 +0200

    styles

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -8,19 +8,9 @@ export default function Quote(randomQuote: QuoteType) {
   const router = useRouter()
   const [fade, setFade] = useState(false)
   return (
-    <section className="flex w-4/5 flex-col items-center justify-center gap-3 lg:w-1/2">
-      <button
-        onClick={() => {
-          setFade(true)
-          setTimeout(() => router.refresh(), 600)
-          setTimeout(() => setFade(false), 1000)
-        }}
-        className="w-32 rounded-xl border bg-transparent p-3"
-      >
-        Get Random Quote
-      </button>
+    <section className="flex h-screen w-[90%] flex-col items-center justify-center gap-3 lg:w-3/4">
       <div
-        className={`flex flex-col items-center justify-center transition-opacity duration-1000 ease-in-out
+        className={`flex grow flex-col items-center justify-center transition-opacity duration-1000 ease-in-out
           ${fade ? "opacity-0" : " opacity-100"}`}
       >
         <div className="flex gap-2">
@@ -33,12 +23,24 @@ export default function Quote(randomQuote: QuoteType) {
           <h1 className="text-lg font-bold">{randomQuote.author}</h1>
         </div>
 
-        <div className="grow">
-          <p className="text-3xl italic">
-            <Balancer>{`"${randomQuote.content}"`}</Balancer>
+        <div className="mt-3">
+          <p className="text-3xl font-bold italic">
+            <Balancer>
+              &ldquo;{randomQuote.content.replace(".", "")}&bdquo;
+            </Balancer>
           </p>
         </div>
       </div>
+      <button
+        onClick={() => {
+          setFade(true)
+          setTimeout(() => router.refresh(), 600)
+          setTimeout(() => setFade(false), 1000)
+        }}
+        className="mb-5 w-44 rounded-xl border bg-transparent p-3"
+      >
+        Get Random Quote
+      </button>
     </section>
   )
 }
