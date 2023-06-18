f619eae9aa1dd4c48a6a1a534852940f0dd78a3b
Zafchiel <bialakropla127.9@o2.pl>
2023-06-19 00:18:47 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Head from "next/head"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -6,6 +7,25 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Random Quote",
   description: "Generated random quote",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 }
 
 export default function RootLayout({
